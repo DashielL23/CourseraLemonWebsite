b9d8a93edbbc99eaef91d55f14c598a437f75e34
DashielL23 <144473374+DashielL23@users.noreply.github.com>
2025-06-03 12:31:04 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Nav from "./components/Nav.js"
+import Header from "./components/Header.js"
+import BookingForm from "./components/BookingForm.js"
+function App() {
+  return (
+    <>
+      <Nav/>
+      <Header/>
+      <BookingForm/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import bannerImg from '../images/restauranfood.jpg'
+const Header = () =>{
+    return(
+        <div className='header'>
+            <section>
+                <div className='banner'>
+                    <h2>Little Lemon</h2>
+                    <h3>Omaha</h3>
+                    <p>We are a traditional, family owned Mediterranean restaurant, focused on home made recipes served 
+                    with a twist!</p>
+                    <Link to="/booking"><button aria-label='On Click'>Reserve Table</button></Link>
+                </div>
+
+                <div className='banner-img'>
+                    <img src={bannerImg} alt=''></img>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Header;
